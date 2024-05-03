a9bb438fbec60853810a0be63b839bb2f962d70e
vnray <ervnray@gmail.com>
2024-05-03 22:24:48 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-for (let i = 0; i < 10; i++) {
-  if (i == 5) {
-    console.log("detected 5");
-    continue;
-  }
+// for (let i = 0; i < 10; i++) {
+//   if (i == 5) {
+//     console.log("detected 5");
+//     continue;
+//   }
+//   console.log(`value of i = ${i}`);
+// }
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(`value of i = ${i}`);
+//   i = i + 2;
+// }
+
+let i = 10;
+
+do {
   console.log(`value of i = ${i}`);
-}
+  i = i + 3;
+} while (i < 15);
